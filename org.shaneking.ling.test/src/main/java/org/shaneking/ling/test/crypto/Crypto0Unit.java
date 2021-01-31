caf49f9d76502ddec676056182eb2d2fcc653a58
ShaneKing <github@shaneking.org>
2021-01-31 22:48:00 +0800

    adjust test case

---
+++ org.shaneking.ling.test/src/main/java/org/shaneking/ling/test/crypto/Crypto0Unit.java
@@ -0,0 +1,6 @@
+package org.shaneking.ling.test.crypto;
+
+public class Crypto0Unit {
+  public static final String I_LOVE_YOU = "ILoveYou";
+  public static final String I_LOVE_YOU__ENCRYPTED = "TcC53mabDUr4WU5NjQZLTw==";
+}
