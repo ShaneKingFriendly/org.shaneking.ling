50ec78b82588162d39a27ad2b1111f24975a2cbe
ShaneKing <github@shaneking.org>
2021-04-29 23:12:07 +0800

    Transactional scenario : Cache sync DB

--- org.shaneking.ling.zero/src/test/java/sktest/ling/zero/util/List0Test.java
+++ org.shaneking.ling.zero/src/test/java/sktest/ling/zero/util/List0Test.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.shaneking.ling.zero.lang.String0;
 import org.shaneking.ling.zero.util.List0;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 
@@ -11,15 +13,26 @@ class List0Test {
 
   @Test
   void newArrayList() {
+    String[] strings = new String[]{String0.Y, String0.N};
     assertAll(
       () -> assertLinesMatch(List0.newArrayList(String0.Y, String0.N), List0.newArrayList(List0.newArrayList(String0.Y, String0.N))),
       () -> assertLinesMatch(List0.newArrayList(String0.Y, String0.N), List0.newArrayList(String0.Y, String0.N)),
-      () -> assertLinesMatch(List0.newArrayList(String0.Y, String0.N), List0.newArrayList(String0.Y, String0.N))
+      () -> assertLinesMatch(List0.newArrayList(String0.Y, String0.N), List0.newArrayList(String0.Y, String0.N)),
+
+      () -> assertLinesMatch(List0.newArrayList(String0.Y, String0.N), List0.newArrayList(strings))
     );
   }
 
   @Test
   void reverse() {
     assertLinesMatch(List0.newArrayList(String0.N, String0.Y), List0.reverse(List0.newArrayList(String0.Y, String0.N)));
+  }
+
+  @Test
+  void retainAll() {
+    List<String> a = List0.newArrayList(String0.Y, String0.N, String0.T);
+    List<String> b = List0.newArrayList(String0.Y, String0.N, String0.F);
+    a.retainAll(b);
+    assertLinesMatch(List0.newArrayList(String0.Y, String0.N), a);
   }
 }
--- org.shaneking.ling.zero/src/test/java/sktest/ling/zero/util/Map0Test.java
+++ org.shaneking.ling.zero/src/test/java/sktest/ling/zero/util/Map0Test.java
@@ -1,8 +1,11 @@
 package sktest.ling.zero.util;
 
 import org.junit.jupiter.api.Test;
+import org.shaneking.ling.zero.lang.String0;
 import org.shaneking.ling.zero.util.List0;
 import org.shaneking.ling.zero.util.Map0;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,5 +21,12 @@ class Map0Test {
         , List0.newArrayList(Map0.newHashMap(List0.newArrayList("a", "b"), List0.newArrayList("a", "b")).values())),
       () -> assertEquals(3, Map0.newHashMap(List0.newArrayList("a", "b", "c"), List0.newArrayList("a", "b")).size())
     );
+  }
+
+  @Test
+  void keySet() {
+    Map<String, String> map = Map0.newHashMap(List0.newArrayList(String0.Y, String0.N), List0.newArrayList(String0.Y, String0.N));
+    map.keySet().remove(String0.Y);
+    assertEquals(1, map.size());
   }
 }
