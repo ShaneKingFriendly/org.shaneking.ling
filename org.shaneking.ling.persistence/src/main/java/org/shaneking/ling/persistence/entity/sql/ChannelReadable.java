ea7ecca94551492d5fa6d7f297af0b6954ec15ed
ShaneKing <github@shaneking.org>
2021-05-12 22:57:32 +0800

    0.241.6.Friendly-SNAPSHOT

--- org.shaneking.ling.persistence/src/main/java/org/shaneking/ling/persistence/entity/sql/ChannelReadable.java
+++ org.shaneking.ling.persistence/src/main/java/org/shaneking/ling/persistence/entity/sql/ChannelReadable.java
@@ -7,7 +7,7 @@ import javax.persistence.Transient;
  * <p>
  * scenario1: one user(userNo) registered in multiple tenants, so multiple tenant resources can be used
  */
-public interface ChannelReadable {
+public interface ChannelReadable extends Tenanted {
   @Transient
   String ERR_CODE__PERMISSION_DENIED = "CHANNEL_READABLE__PERMISSION_DENIED";
 }
