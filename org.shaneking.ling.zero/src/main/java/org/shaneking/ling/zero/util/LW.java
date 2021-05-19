5755801c658660d1d96d1e36f78eedba643ed0dd
ShaneKing <github@shaneking.org>
2021-05-19 19:56:16 +0800

    ListWrapper and MapWrapper

---
+++ org.shaneking.ling.zero/src/main/java/org/shaneking/ling/zero/util/LW.java
@@ -0,0 +1,41 @@
+package org.shaneking.ling.zero.util;
+
+import java.util.List;
+
+public class LW<E> {
+  private final List<E> list = List0.newArrayList();
+
+  public static <E> LW<E> wrap(E e) {
+    LW<E> lw = new LW<>();
+    if (e != null) {
+      lw.list.add(e);
+    }
+    return lw;
+  }
+
+  public static <E> LW<E> wrap(List<E> l) {
+    LW<E> lw = new LW<>();
+    if (l != null) {
+      lw.list.addAll(l);
+    }
+    return lw;
+  }
+
+  public LW<E> add(E e) {
+    if (e != null) {
+      list.add(e);
+    }
+    return this;
+  }
+
+  public LW<E> addAll(List<E> l) {
+    if (l != null) {
+      list.addAll(l);
+    }
+    return this;
+  }
+
+  public List<E> list() {
+    return list;
+  }
+}
---
+++ org.shaneking.ling.zero/src/main/java/org/shaneking/ling/zero/util/MW.java
@@ -0,0 +1,41 @@
+package org.shaneking.ling.zero.util;
+
+import java.util.Map;
+
+public class MW<K, V> {
+  private final Map<K, V> map = Map0.newHashMap();
+
+  public static <K, V> MW<K, V> wrap(K k, V v) {
+    MW<K, V> mw = new MW<>();
+    if (k != null) {
+      mw.map.put(k, v);
+    }
+    return mw;
+  }
+
+  public static <K, V> MW<K, V> wrap(Map<K, V> m) {
+    MW<K, V> mw = new MW<>();
+    if (m != null) {
+      mw.map.putAll(m);
+    }
+    return mw;
+  }
+
+  public MW<K, V> put(K k, V v) {
+    if (k != null) {
+      map.put(k, v);
+    }
+    return this;
+  }
+
+  public MW<K, V> putAll(Map<K, V> m) {
+    if (m != null) {
+      map.putAll(m);
+    }
+    return this;
+  }
+
+  public Map<K, V> map() {
+    return map;
+  }
+}
