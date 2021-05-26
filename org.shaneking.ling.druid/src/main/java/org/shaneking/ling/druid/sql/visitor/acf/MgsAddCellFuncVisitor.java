8b37ba6a89a4b02583d0e70c80a561dc8e8a5a37
ShaneKing <github@shaneking.org>
2021-05-27 00:25:29 +0800

    adjust maven modules

--- org.shaneking.ling.druid/src/main/java/org/shaneking/ling/druid/sql/visitor/acf/MgsAddCellFuncVisitor.java
+++
@@ -1,6 +0,0 @@
-package org.shaneking.ling.druid.sql.visitor.acf;
-
-import com.alibaba.druid.sql.visitor.SQLASTVisitor;
-
-public interface MgsAddCellFuncVisitor extends SQLASTVisitor {
-}
--- org.shaneking.ling.druid/src/main/java/org/shaneking/ling/druid/sql/visitor/pts/MgsParseTableStatVisitor.java
+++
@@ -1,6 +0,0 @@
-package org.shaneking.ling.druid.sql.visitor.pts;
-
-import com.alibaba.druid.sql.visitor.SQLASTVisitor;
-
-public interface MgsParseTableStatVisitor extends SQLASTVisitor {
-}
