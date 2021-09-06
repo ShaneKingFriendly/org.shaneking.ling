dfd1d12db0fed0e4c07fbe1e92aa6daba31cedb1
ShaneKing <github@shaneking.org>
2021-09-07 00:09:03 +0800

    org.shaneking.ling.zero adjust filed and method sort

--- org.shaneking.ling.zero/src/test/java/sktest/ling/zero/util/concurrent/EvictingQueueTest.java
+++ org.shaneking.ling.zero/src/test/java/sktest/ling/zero/util/concurrent/EvictingQueueTest.java
@@ -8,70 +8,69 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class EvictingQueueTest {
 
   @Test
   void addAll() {
-    EvictingQueue<Integer> evictingQueue = new EvictingQueue<>(3);
-    for (int i = 0; i < 8; i++) {
-      evictingQueue.put(i);
-    }
-    evictingQueue.addAll(List0.newArrayList(10, 11, 12, 13));
-    assertEquals("[11, 12, 13]", String.valueOf(evictingQueue));
+    assertAll(
+      () -> {
+        EvictingQueue<Integer> evictingQueue = new EvictingQueue<>(3);
+        for (int i = 0; i < 8; i++) {
+          evictingQueue.put(i);
+        }
+        evictingQueue.addAll(List0.newArrayList(10, 11, 12, 13));
+        assertEquals("[11, 12, 13]", String.valueOf(evictingQueue));
+      },
+      () -> {//addAll4bitwise
+        AtomicLong atomicLong = new AtomicLong(0);
+        Supplier<Boolean> supplier = () -> {
+          atomicLong.incrementAndGet();
+          return true;
+        };
+        boolean b = false;
+        assertEquals(false, b & supplier.get());
+        assertEquals(1, atomicLong.get());//executed
+        assertEquals(false, b && supplier.get());
+        assertEquals(1, atomicLong.get());//unExecuted
+      }
+    );
   }
 
   @Test
-  void addAll4bitwise() {
-    AtomicLong atomicLong = new AtomicLong(0);
-    Supplier<Boolean> supplier = () -> {
-      atomicLong.incrementAndGet();
-      return true;
-    };
-    boolean b = false;
-    assertEquals(false, b & supplier.get());
-    assertEquals(1, atomicLong.get());//executed
-    assertEquals(false, b && supplier.get());
-    assertEquals(1, atomicLong.get());//unExecuted
-  }
-
-  @Test
-  void offer4timeout() {
-    EvictingQueue<Integer> evictingQueue = new EvictingQueue<>(3);
-    for (int i = 0; i < 8; i++) {
-      evictingQueue.offer(i, 1000, TimeUnit.MILLISECONDS);
-    }
-    assertEquals("[5, 6, 7]", String.valueOf(evictingQueue));
-  }
-
-  @Test
-  void offer4timeout4exception() {
-    EvictingQueue<Integer> evictingQueue = new EvictingQueue<>(3);
-    for (int i = 0; i < 8; i++) {
-      final int fi = i;
-      assertDoesNotThrow(() -> evictingQueue.offer(fi, 1, TimeUnit.NANOSECONDS));
-    }
+  void offer() {
+    assertAll(
+      () -> {//offer4timeout
+        EvictingQueue<Integer> evictingQueue = new EvictingQueue<>(3);
+        for (int i = 0; i < 8; i++) {
+          evictingQueue.offer(i, 1000, TimeUnit.MILLISECONDS);
+        }
+        assertEquals("[5, 6, 7]", String.valueOf(evictingQueue));
+      },
+      () -> {//offer4timeout4exception
+        EvictingQueue<Integer> evictingQueue = new EvictingQueue<>(3);
+        for (int i = 0; i < 8; i++) {
+          final int fi = i;
+          assertDoesNotThrow(() -> evictingQueue.offer(fi, 1, TimeUnit.NANOSECONDS));
+        }
 //    assertThrows(ZeroException.class, () -> evictingQueue.offer(9, 1, TimeUnit.NANOSECONDS, false));
-    assertEquals("[5, 6, 7]", String.valueOf(evictingQueue));
-  }
-
-  @Test
-  void offer4withCapacity() {
-    EvictingQueue<Integer> evictingQueue = new EvictingQueue<>(3);
-    for (int i = 0; i < 8; i++) {
-      evictingQueue.offer(i);
-    }
-    assertEquals("[5, 6, 7]", String.valueOf(evictingQueue));
-  }
-
-  @Test
-  void offer4withoutCapacity() {
-    EvictingQueue<Integer> evictingQueue = new EvictingQueue<>();
-    for (int i = 0; i < 8; i++) {
-      evictingQueue.add(i);
-    }
-    assertEquals("[0, 1, 2, 3, 4, 5, 6, 7]", String.valueOf(evictingQueue));
+        assertEquals("[5, 6, 7]", String.valueOf(evictingQueue));
+      },
+      () -> {//offer4withCapacity
+        EvictingQueue<Integer> evictingQueue = new EvictingQueue<>(3);
+        for (int i = 0; i < 8; i++) {
+          evictingQueue.offer(i);
+        }
+        assertEquals("[5, 6, 7]", String.valueOf(evictingQueue));
+      },
+      () -> {//offer4withoutCapacity
+        EvictingQueue<Integer> evictingQueue = new EvictingQueue<>();
+        for (int i = 0; i < 8; i++) {
+          evictingQueue.add(i);
+        }
+        assertEquals("[0, 1, 2, 3, 4, 5, 6, 7]", String.valueOf(evictingQueue));
+      }
+    );
   }
 }
