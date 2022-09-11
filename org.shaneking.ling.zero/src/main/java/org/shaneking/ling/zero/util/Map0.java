97cb4f7724eb0b6b8fe4493af595b2968d05299b
ShaneKing <github@shaneking.org>
2022-09-11 14:41:13 +0800

    0.255.12.Friendly-SNAPSHOT for AzollaBOT@0.0.2.AzollaLab

--- org.shaneking.ling.zero/src/main/java/org/shaneking/ling/zero/util/Map0.java
+++ org.shaneking.ling.zero/src/main/java/org/shaneking/ling/zero/util/Map0.java
@@ -21,6 +21,14 @@ public class Map0 {
     return rtnMap;
   }
 
+  public static <I> HashMap<I, I> newHashMap(I... ii) {
+    HashMap<I, I> rtnMap = Map0.newHashMap();
+    for (int i = 1; i < ii.length; i = i + 2) {
+      rtnMap.put(ii[i - 1], ii[i]);
+    }
+    return rtnMap;
+  }
+
   public static <K, V> HashMap<K, V> newHashMap(@NonNull List<K> keys, @NonNull List<V> values) {
     HashMap<K, V> rtn = Map0.newHashMap();
     if (keys.size() > values.size()) {
